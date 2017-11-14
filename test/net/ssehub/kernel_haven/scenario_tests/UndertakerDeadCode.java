8329cfa60fda3178d0ee438c6f092d3f968e3a5f
Elscha <elscha@sse.uni-hildesheim.de>
2017-11-14 09:21:29 +0100

    Moved Abstract ScenarioTests from ScenarioTests to KH-Test bundle up.

--- test/net/ssehub/kernel_haven/scenario_tests/UndertakerDeadCode.java
+++ test/net/ssehub/kernel_haven/scenario_tests/UndertakerDeadCode.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 import org.junit.runner.RunWith;
 
+import net.ssehub.kernel_haven.AbstractScenarioTest;
 import net.ssehub.kernel_haven.util.io.csv.CsvReader;
 
 /**
