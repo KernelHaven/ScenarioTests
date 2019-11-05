9e9b5b6dd27c33b8248282668c8f7e3d44c63cea
Elscha <elscha@sse.uni-hildesheim.de>
2019-11-05 15:05:09 +0100

    Fixes test for Distance weight; Adds an additional test for that weight

--- test/net/ssehub/kernel_haven/scenario_tests/metrics/weights/FeatureDistanceTests.java
+++ test/net/ssehub/kernel_haven/scenario_tests/metrics/weights/FeatureDistanceTests.java
@@ -32,6 +32,7 @@ import net.ssehub.kernel_haven.metric_haven.code_metrics.VariablesPerFunction;
 import net.ssehub.kernel_haven.metric_haven.code_metrics.VariablesPerFunction.VarType;
 import net.ssehub.kernel_haven.metric_haven.metric_components.config.FeatureDistanceType;
 import net.ssehub.kernel_haven.metric_haven.metric_components.config.MetricSettings;
+import net.ssehub.kernel_haven.scenario_tests.metrics.AbstractCodeMetricTests;
 import net.ssehub.kernel_haven.scenario_tests.metrics.AbstractParameterizedTests;
 import net.ssehub.kernel_haven.test_utils.PseudoVariabilityExtractor;
 import net.ssehub.kernel_haven.variability_model.SourceLocation;
@@ -46,14 +47,35 @@ import net.ssehub.kernel_haven.variability_model.VariabilityVariable;
 @RunWith(Parameterized.class)
 public class FeatureDistanceTests extends AbstractParameterizedTests {
 
-    private static final Properties VAR_INTERNAL = new Properties();
+    private static final Properties DISTANCE_SETTING = new Properties();
     
     static {
-        VAR_INTERNAL.setProperty(MetricSettings.VARIABLE_TYPE_SETTING.getKey(), VarType.INTERNAL.name());
-        VAR_INTERNAL.setProperty(MetricSettings.LOCATION_DISTANCE_SETTING.getKey(),
+        DISTANCE_SETTING.setProperty(MetricSettings.VARIABLE_TYPE_SETTING.getKey(), VarType.INTERNAL.name());
+        DISTANCE_SETTING.setProperty(MetricSettings.LOCATION_DISTANCE_SETTING.getKey(),
             FeatureDistanceType.SHORTEST_DISTANCE.name());
-        
+        DISTANCE_SETTING.setProperty("source_tree", AbstractCodeMetricTests.TESTDATA.getAbsolutePath());  
     }
+    
+    /**
+     * Specification of variables to use for the test.
+     * @author El-Sharkawy
+     */
+    private static class VarSpec {
+        private String variableName;
+        private File path;
+        
+        /**
+         * Sole constructor.
+         * @param variableName The name of the variable.
+         * @param path The folder where the variable should be specified.
+         */
+        private VarSpec(String variableName, File path) {
+            this.variableName = variableName;
+            this.path = path;
+        }
+    }
+    
+    private Properties prop;
     
     /**
      * Retrieves values from {@link #getParameters()}, creates, and executes the test.
@@ -61,24 +83,29 @@ public class FeatureDistanceTests extends AbstractParameterizedTests {
      * @param testedFunctionName The function to test
      * @param expectedLineNo The expected starting line number of the function
      * @param expectedResultValue The expected value of the metric to compute.
+     * @param variables Specification of variables to use for the test.
      */
     public FeatureDistanceTests(String fileName, String testedFunctionName, int expectedLineNo,
-        double expectedResultValue) {
+        double expectedResultValue, VarSpec[] variables) {
         
         super(fileName, testedFunctionName, expectedLineNo, expectedResultValue);
         
         // Configuration of variability model
-        // Variable A's distance is 2
-        VariabilityVariable varA = new VariabilityVariable("A", "bool");
-        SourceLocation srcVarA = new SourceLocation(new File(getTestFile().getAbsoluteFile().getParentFile(),
-            "A.varModel"), 1);
-        varA.addLocation(srcVarA);
-        // Variable B's distance is 3
-        VariabilityVariable varB = new VariabilityVariable("B", "bool");
-        SourceLocation srcVarB = new SourceLocation(new File(getTestFile().getAbsoluteFile(), "B.varModel"), 1);
-        varB.addLocation(srcVarB);
+        VariabilityVariable[] vars = new VariabilityVariable[variables.length];
+        for (int i = 0; i < variables.length; i++) {
+            vars[i] = new VariabilityVariable(variables[i].variableName, "bool");
+            SourceLocation location = new SourceLocation(new File(variables[i].path, variables[i].variableName
+                + ".varModel"), 1);
+            vars[i].addLocation(location);
+            
+            System.out.println(location.getSource().getAbsolutePath());
+        }
         
-        PseudoVariabilityExtractor.configure(new File("mocked_varModel.dimacs"), varA, varB);
+        prop = new Properties();
+        prop.putAll(DISTANCE_SETTING);
+        prop.setProperty("code.extractor.files", fileName);
+        
+        PseudoVariabilityExtractor.configure(new File("mocked_varModel.dimacs"), vars);
         PseudoVariabilityExtractor.setAttributes(Attribute.SOURCE_LOCATIONS);
     }
     
@@ -94,8 +121,24 @@ public class FeatureDistanceTests extends AbstractParameterizedTests {
      */
     @Parameters(name = "Feature Distance Weight: {1}")
     public static Collection<Object[]> getParameters() {
+        File folder = new File("VariabilityFunctions.c").getParentFile();
+       
         return Arrays.asList(new Object[][] {
-            {"VariabilityFunctions.c", "funcVarNesting", 21, 5}
+            {"VariabilityFunctions.c", "funcVarNesting", 21, 1, new VarSpec[] {
+                // Variable A's distance is 0 (same folder as code file)
+                new VarSpec("A", folder),
+                // Variable B's distance is 1 (sub folder of code folder)
+                new VarSpec("B", new File("subFolder"))
+                }
+            },
+            
+            {"subFolderForDistance/VariabilityFunctions.c", "funcVarNesting", 21, 3, new VarSpec[] {
+                // Variable A's distance is 1 (parent folder of code folder)
+                new VarSpec("A", folder),
+                // Variable B's distance is 2 (parallel to code folder (one up and down to another folder))
+                new VarSpec("B", new File("subFolder"))
+                }
+            }
         });
     }
     
@@ -104,7 +147,7 @@ public class FeatureDistanceTests extends AbstractParameterizedTests {
      */
     @Test
     public void test() {
-        super.test(VAR_INTERNAL);
+        super.test(prop);
     }
 
     @Override
